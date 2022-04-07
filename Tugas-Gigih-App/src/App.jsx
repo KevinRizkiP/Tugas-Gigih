cdcd9af564484e67fc55f45990c39c84c23e3862
Kevin Rizki <kevinrizki002@gmail.com>
2022-04-07 18:40:21 +0700

    Homework Module 4 Session 2

---
+++ Tugas-Gigih-App/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes,Navigate  } from "react-router-dom";
+import { useSelector } from "react-redux";
+import CreatePlaylistForm from "./components/CreatePlaylistForm";
+import SearchGroup from "./pages/SearchGroup/SearchGroup";
+
+function App() {
+  const isAuthorize = useSelector((state) => state.auth.isAuthorize);
+  return (
+    <Routes>
+      {!isAuthorize && <Route path="/" element={<Home />} />}
+      {isAuthorize && (
+        <>
+          <Route path="/create-playlist" element={<CreatePlaylistForm />} />
+          <Route path="/search" element={<SearchGroup />} />
+        </>
+      )}
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ Tugas-Gigih-App/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <ToastContainer />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
